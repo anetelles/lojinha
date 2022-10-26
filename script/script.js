e57d0414851cc2451c6b1acd7c82baf6ad506324
Cristiane Telles <108342343+anetelles@users.noreply.github.com>
2022-10-26 16:53:28 -0300

    v.0.7

---
+++ script/script.js
@@ -0,0 +1,95 @@
+// var nome = prompt("Seja bem vindo(a), qual o seu nome?");
+
+// document.write("Olá, " + nome + ".");
+
+//Primeiro card:
+function showTexto() {
+    document.getElementById("texto").innerHTML = "Comprar";
+}
+function phaseTexto() {
+    document.getElementById("texto").innerHTML = "Talvez sim...? 🤔";
+} 
+function resetTexto(){
+    document.getElementById("texto").innerHTML = "Comprar";
+}
+
+//Segundo card:
+function showTexto1() {
+    document.getElementById("texto1").innerHTML = "Comprar";
+}
+function phaseTexto1() {
+    document.getElementById("texto1").innerHTML = "Por que não? 🤔";
+} 
+function resetTexto1() {
+    document.getElementById("texto1").innerHTML = "Comprar";
+} 
+
+//Terceiro card:
+function showTexto2() {
+    document.getElementById("texto2").innerHTML = "Comprar";
+}
+function phaseTexto2() {
+    document.getElementById("texto2").innerHTML = "Talvez sim...? 🤔";
+} 
+function resetTexto2() {
+    document.getElementById("texto2").innerHTML = "Comprar";
+} 
+
+//Quarto card:
+function showTexto3() {
+    document.getElementById("texto3").innerHTML = "Comprar";
+}
+function phaseTexto3() {
+    document.getElementById("texto3").innerHTML = "Talvez sim...? 🤔";
+} 
+function resetTexto3() {
+    document.getElementById("texto3").innerHTML = "Comprar";
+} 
+
+//Quinto card:
+function showTexto4() {
+    document.getElementById("texto4").innerHTML = "Comprar";
+}
+function phaseTexto4() {
+    document.getElementById("texto4").innerHTML = "Talvez sim...? 🤔";
+} 
+function resetTexto4() {
+    document.getElementById("texto4").innerHTML = "Comprar";
+} 
+
+//Sexto card:
+function showTexto5() {
+    document.getElementById("texto5").innerHTML = "Comprar";
+}
+function phaseTexto5() {
+    document.getElementById("texto5").innerHTML = "Talvez sim...? 🤔";
+} 
+function resetTexto5() {
+    document.getElementById("texto5").innerHTML = "Comprar";
+} 
+
+//Sexto card:
+function showTexto6() {
+    document.getElementById("texto6").innerHTML = "Comprar";
+}
+function phaseTexto6() {
+    document.getElementById("texto6").innerHTML = "Talvez sim...? 🤔";
+} 
+function resetTexto6() {
+    document.getElementById("texto6").innerHTML = "Comprar";
+} 
+
+//Sexto card:
+function showTexto7() {
+    document.getElementById("texto7").innerHTML = "Comprar";
+}
+function phaseTexto7() {
+    document.getElementById("texto7").innerHTML = "Talvez sim...? 🤔";
+} 
+function resetTexto7() {
+    document.getElementById("texto7").innerHTML = "Comprar";
+} 
+
+
+
+
